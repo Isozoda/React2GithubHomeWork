aa51985a67b0d33fcb24522927d045f046933028
Isozoda09 <ibrohimjunusov@gmail.com>
2026-03-26 15:32:11 +0500

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import Home from "./page/Home";
-import InfoPage from "./page/infoPage";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import React from 'react'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/info/:id" element={<InfoPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      app
+    </div>
   )
 }
 
